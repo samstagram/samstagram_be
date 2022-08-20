4ffaff20519a3b4cb5b0f8268fdcb20b0524b12c
2m <philomonx1@gmail.com>
2022-08-21 07:12:51 +0900

    [수정] 누락된 맴버 레포지토리 추가

--- src/main/java/com/hanghae99/samstargram_be/repository/MemberRepository.java
+++ src/main/java/com/hanghae99/samstargram_be/repository/MemberRepository.java
@@ -11,4 +11,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	Optional<Member> findByUsername(String username);
 	boolean existsByUsername(String username);
+	Optional<Member> findByUseremail(String useremail);
+
+	Optional<Member> findBySocialId(String googleSocialID);
 }
